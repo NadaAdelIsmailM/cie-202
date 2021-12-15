dfa3c6b5dc656fd9aa11034b3c33d89d3f48607a
Farreeda <94711213+Farreeda@users.noreply.github.com>
2021-12-15 17:20:01 +0200

    Update Loadcircuit.h

--- Loadcircuit.h
+++ Loadcircuit.h
@@ -2,20 +2,19 @@
 
 #include "Action.h"
 #include "../ApplicationManager.h"
-
+#include"..\Components\Component.h"
 #include <iostream>
 #include<fstream>
 
 using namespace std;
 
 class Loadcircuit : public Action {
-public:
+ifstream myfile;
 	//Loadcircuit(void);
 public:
 	Loadcircuit(ApplicationManager* pApp);
 	
 	virtual void Execute();
-	virtual void loadcircuit();
 	virtual void Undo();
 	virtual void Redo();
 	virtual ~Loadcircuit(void);
